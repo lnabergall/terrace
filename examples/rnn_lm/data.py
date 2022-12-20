80f48053ab34e60c7679c5de7ddb3404e424a2a0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 10:54:52 +0000

    Adding tarfile member sanitization to extractall()

--- examples/rnn_lm/data.py
+++ examples/rnn_lm/data.py
@@ -32,7 +32,26 @@ def download_corpus(directory):
             corpus_file.write(response.content)
         # extract
         with tarfile.open(corpus_file_path, "r:gz") as corpus_tar:
-            corpus_tar.extractall(directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(corpus_tar, directory)
 
 
 def get_original_vocab(directory):
